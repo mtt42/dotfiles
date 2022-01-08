b1dccd02df5f0406e6cb86982f772f49aaecac06
MoreThanThis42 <mtt42@protonmail.com>
2022-01-08 20:25:31 +0100

    replaced init.vim by lua config

---
+++ .config/nvim/lua/settings/init.lua
@@ -0,0 +1,33 @@
+-- settings
+local set = vim.opt
+
+-- gui
+set.showmode       = false
+set.cursorline     = true
+set.number         = true
+set.relativenumber = true
+set.termguicolors  = true
+set.splitright     = true
+set.splitbelow     = true
+set.mouse          = 'a'
+set.encoding       = 'utf-8'
+set.colorcolumn    = '80'
+set.signcolumn     = 'number'
+set.background     = 'dark'
+
+-- tabs
+set.list           = true
+set.listchars      = 'tab:»-»'
+set.tabstop        = 4
+set.softtabstop    = 2
+set.shiftwidth     = 2
+set.expandtab      = true
+set.laststatus     = 0
+
+-- fs
+set.undofile       = true
+set.undolevels     = 1000
+set.undoreload     = 10000
+set.autowrite      = true
+set.hidden         = true
+set.swapfile       = false
