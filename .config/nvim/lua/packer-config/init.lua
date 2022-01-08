f05aafaf6a1c857ec7f3cd58d606de03a914ca89
MoreThanThis42 <mtt42@protonmail.com>
2022-01-08 20:54:04 +0100

    added termtoggle plugin

--- .config/nvim/lua/packer-config/init.lua
+++ .config/nvim/lua/packer-config/init.lua
@@ -3,5 +3,6 @@ return require'packer'.startup(function()
   use 'EdenEast/nightfox.nvim'
   use 'kyazdani42/nvim-web-devicons'
   use 'kyazdani42/nvim-tree.lua'
+  use 'akinsho/toggleterm.nvim'
 end)
 
