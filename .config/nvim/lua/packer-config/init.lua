4155627e1e59f72aceeabfe529dd607196b7c4a0
MoreThanThis42 <mtt42@protonmail.com>
2022-01-09 18:11:10 +0100

    added autocolpete and lsp

--- .config/nvim/lua/packer-config/init.lua
+++ .config/nvim/lua/packer-config/init.lua
@@ -4,5 +4,10 @@ return require'packer'.startup(function()
   use 'kyazdani42/nvim-web-devicons'
   use 'kyazdani42/nvim-tree.lua'
   use 'akinsho/toggleterm.nvim'
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp' 
+  use 'saadparwaiz1/cmp_luasnip' 
+  use 'L3MON4D3/LuaSnip'
 end)
 
